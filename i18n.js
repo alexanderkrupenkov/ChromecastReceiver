eeee03975866240b9ed29ae05e0b22bdf780299e
bernikowich-cedoni <timur@netcosports.com>
2016-02-15 18:00:08 +0300

    Initial commit.

---
+++ i18n.js
@@ -0,0 +1,26 @@
+var i18n = {
+  en : {
+    connectionError : 'There a problem with the WiFi connection. Please check WiFi settings or the router itself.',
+    tooManyConnectionsError : 'You have reached the maximum number of simultaneous connections. We invite you to leave and come back on this page in order to watch the Live channels.',
+    permissionError : 'You do not have permission.',
+    nowCasting : 'Now Casting: ',
+    nowCastingEmpty : 'Now Casting',
+    readyToCast : 'Ready to cast',
+  },
+  fr : {
+    connectionError : 'Problème de connexion WIFI. Veuillez vérifier vos paramètres WIFI',
+    tooManyConnectionsError : 'Vous avez atteint le nombre maximum de connexions simultanées. Nous vous invitons à quitter puis revenir sur la page du direct.',
+    permissionError : 'Vous n’avez pas l’autorisation nécessaire.',
+    nowCasting : 'Maintenant en direct: ',
+    nowCastingEmpty : 'Maintenant en direct',
+    readyToCast : 'Prêt à la diffusion en direct',
+  },
+  ru : {
+    connectionError : 'Проблема с соединением Wi-Fi. Проверьте настройки роутера.',
+    tooManyConnectionsError : 'Достигнут максимум разрешенных соединений. Перезайдите на страницу для просмотра передач.',
+    permissionError : 'У вас нет доступа',
+    nowCasting : 'Сейчас транслируется : ',
+    nowCastingEmpty : 'Сейчас транслируется',
+    readyToCast : 'Готов к трансляции',
+  }
+};
